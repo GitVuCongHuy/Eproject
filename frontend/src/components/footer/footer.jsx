dd2f34cdf54a8059f6b5e8746f23c32907cac612
Tietve <thienhuu2005@gmail.com>
2025-06-18 15:07:58 +0700

    mockdata dashboard

---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,1 @@
+// code footer ở đây
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}", // đảm bảo Tailwind hoạt động trong src/
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
