196855ba74a0169d6b6bd0ac51972b43b11cb49d
kiensn123 <kien2005kiensn@gmail.com>
2025-06-21 20:04:54 +0700

    ccc

--- backend/Controllers/Transaction_passwordsController.cs
+++ backend/Controllers/Transaction_passwordsController.cs
@@ -20,79 +20,12 @@ public class Transaction_passwordsController : Controller
         _emailHelper = emailHelper;
     }
 
-    // [HttpPost("create")]
-    // public async Task<IActionResult> Create_Transaction_passwords([FromBody] Transaction_passwordsView view)
-    // {
-    //     try
-    //     {
-            
-    //         var token = _jwtTokenHelper.GetBearerToken(HttpContext);
-
-    //         if (string.IsNullOrEmpty(token))
-    //         {
-    //             return BadRequest(new ApiError
-    //             {
-    //                 Status = 400,
-    //                 Error = "There are no tokens yet",
-    //                 Message = "Chưa có token được truyền vào"
-    //             });
-    //         }
-
-
-
-    //         var principal = _jwtTokenHelper.DecodeToken(token); 
-    //         var customerId = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
-    //         if (customerId == null)
-    //         {
-    //             return BadRequest(new ApiError
-    //             {
-    //                 Status = 400,
-    //                 Error = "Token Expired",
-    //                 Message = "Phiên đăng nhập đã hết hạn , vui lòng đăng nhập lại"
-    //             });
-    //         }
-    //         int customerId2 = int.Parse(customerId);
-            
-    //         var TransactionPassword_ = await _context.transaction_Passwords.FirstOrDefaultAsync(x => x.CustomerId == customerId2);
-    //         if (TransactionPassword_ == null)
-    //         {
-    //             var TransactionPassword_new = new Transaction_passwords
-    //             {
-    //                 CustomerId = customerId2,
-    //                 TransactionPassword = view.TransactionPassword,
-    //             };
-    //             _context.transaction_Passwords.Add(TransactionPassword_new);
-    //             await _context.SaveChangesAsync();
-    //         }
-    //         else
-    //         {
-    //             TransactionPassword_.TransactionPassword = view.TransactionPassword;
-    //             await _context.SaveChangesAsync();
-    //         }
-
-
-    //         return Ok(new ApiResponse<object>
-    //         {
-    //             Status = 200,
-    //             Message = "Log in successfully",
-    //             Data = new { Customer_id = customerId }  // ✅ trả token dạng string
-    //         });
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         return StatusCode(500, $"Lỗi server: {ex.Message}");
-    //     }
-    // }
-
-
-
-
-    [HttpPost("check")]
-    public async Task<IActionResult> Check_Transaction_passwords([FromBody] Transaction_passwordsView view)
+    [HttpPost("create")]
+    public async Task<IActionResult> Create_Transaction_passwords([FromBody] Transaction_passwordsView view)
     {
         try
         {
+            
             var token = _jwtTokenHelper.GetBearerToken(HttpContext);
 
             if (string.IsNullOrEmpty(token))
@@ -104,23 +37,90 @@ public class Transaction_passwordsController : Controller
                     Message = "Chưa có token được truyền vào"
                 });
             }
-            var principal = _jwtTokenHelper.DecodeToken(token);
+
+
+
+            var principal = _jwtTokenHelper.DecodeToken(token); 
             var customerId = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (customerId == null)
+            {
+                return BadRequest(new ApiError
+                {
+                    Status = 400,
+                    Error = "Token Expired",
+                    Message = "Phiên đăng nhập đã hết hạn , vui lòng đăng nhập lại"
+                });
+            }
             int customerId2 = int.Parse(customerId);
-
-
+            
             var TransactionPassword_ = await _context.transaction_Passwords.FirstOrDefaultAsync(x => x.CustomerId == customerId2);
+            if (TransactionPassword_ == null)
+            {
+                var TransactionPassword_new = new Transaction_passwords
+                {
+                    CustomerId = customerId2,
+                    TransactionPassword = view.TransactionPassword,
+                };
+                _context.transaction_Passwords.Add(TransactionPassword_new);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                TransactionPassword_.TransactionPassword = view.TransactionPassword;
+                await _context.SaveChangesAsync();
+            }
 
 
-
-
-
-
+            return Ok(new ApiResponse<object>
+            {
+                Status = 200,
+                Message = "Log in successfully",
+                Data = new { Customer_id = customerId }  // ✅ trả token dạng string
+            });
         }
         catch (Exception ex)
         {
             return StatusCode(500, $"Lỗi server: {ex.Message}");
         }
-
     }
+
+
+
+
+    // [HttpPost("check")]
+    // public async Task<IActionResult> Check_Transaction_passwords([FromBody] Transaction_passwordsView view)
+    // {
+    //     try
+    //     {
+    //         var token = _jwtTokenHelper.GetBearerToken(HttpContext);
+
+    //         if (string.IsNullOrEmpty(token))
+    //         {
+    //             return BadRequest(new ApiError
+    //             {
+    //                 Status = 400,
+    //                 Error = "There are no tokens yet",
+    //                 Message = "Chưa có token được truyền vào"
+    //             });
+    //         }
+    //         var principal = _jwtTokenHelper.DecodeToken(token);
+    //         var customerId = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    //         int customerId2 = int.Parse(customerId);
+
+
+    //         var TransactionPassword_ = await _context.transaction_Passwords.FirstOrDefaultAsync(x => x.CustomerId == customerId2);
+
+
+
+
+
+
+    //     }
+    //     catch (Exception ex)
+    //     {
+    //         return StatusCode(500, $"Lỗi server: {ex.Message}");
+    //     }
+
+    // }
 }
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a65257718c81f73543312a18a0df727f5676bfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9417a912d51d6f1e42032da2a5bd9da8a0dd65f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
