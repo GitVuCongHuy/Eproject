8a65257718c81f73543312a18a0df727f5676bfa
kiensn123 <kien2005kiensn@gmail.com>
2025-06-21 15:59:40 +0700

    Day

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c12e6ae9148e43f73640c65ad70996f62d5f45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17993bb5d365dfc544d1470475d64465b5a2de40")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
