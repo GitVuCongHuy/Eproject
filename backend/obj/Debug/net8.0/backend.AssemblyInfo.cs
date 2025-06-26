bb9581017ce43dbd60f0fae7b3ba0660403e1ff9
GitVuCongHuy <vuconghuy.fpt@gmail.com>
2025-06-26 13:54:20 +0700

    aaaa

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c87ac18b1964cae37a191f6408766d12c00ae8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b16b8f0a5eeca394df67c6674ec032f69f64bfb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
