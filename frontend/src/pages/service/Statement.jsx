3e081b28e88013f0f1203818c5313ee0867fe00d
Tietve <156795036+Tietve@users.noreply.github.com>
2025-07-02 21:58:15 +0700

    Update Statement.jsx nhánh

--- frontend/src/pages/service/Statement.jsx
+++ frontend/src/pages/service/Statement.jsx
@@ -21,7 +21,7 @@ import {
   ArrowBack
 } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from 'src/context/Context';
+import { useAuth } from '../../context/context';
 
 const primaryBlack = '#1a1a1a';
 const mediumGray = '#6b7280';
